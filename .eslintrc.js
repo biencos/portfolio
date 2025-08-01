f30fd9c54945674928f2a4c01502e14bbff9c56e
biencos <biencos.dev@gmail.com>
2025-08-01 20:23:10 +0200

    chore: add ESLint and Prettier configuration
    - Add ESLint with React and hooks rules
    - Configure Prettier for consistent formatting
    - Add ignore patterns for build directories

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    // Prettier rules
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    
+    // React rules
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+    'react/prop-types': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error',
+    
+    // React Hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // General rules
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-undef': 'error',
+    
+    // Best practices
+    'eqeqeq': 'error',
+    'curly': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'build/',
+    'node_modules/',
+    'public/',
+    '*.config.js',
+    'craco.config.js',
+  ],
+};
