ea5866e4fde9fe181e8387515924ac984a0aa030
biencos <biencos.dev@gmail.com>
2025-08-01 21:50:00 +0200

    chore: format existing code and add PropTypes
    - Apply consistent formatting to all source files
    - Add PropTypes validation to components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import './Navbar.css';
 
 const Navbar = () => (
-  <div data-role="Header" className="navbar-container">
-    <div className="navbar">
-      <img alt="Portfolio logo" src="/logo_dark.svg" className="navbar-logo" />
-      <div className="navbar-links">
-        <a href="#home" className="navbar-link">Home</a>
-        <a href="#services" className="navbar-link">Services</a>
-        <a href="#experience" className="navbar-link">Experience</a>
-        <a href="#contact" className="navbar-link">Contact</a>
+  <div data-role='Header' className='navbar-container'>
+    <div className='navbar'>
+      <img alt='Portfolio logo' src='/logo_dark.svg' className='navbar-logo' />
+      <div className='navbar-links'>
+        <a href='#home' className='navbar-link'>
+          Home
+        </a>
+        <a href='#services' className='navbar-link'>
+          Services
+        </a>
+        <a href='#experience' className='navbar-link'>
+          Experience
+        </a>
+        <a href='#contact' className='navbar-link'>
+          Contact
+        </a>
       </div>
     </div>
   </div>
