7c28fedf6d307b847902823ea949808a6e49156a
biencos <biencos.dev@gmail.com>
2025-08-24 14:32:29 +0200

    feat: add hamburger menu for mobile navigation
    - Add animated SVG hamburger icon
    - Implement responsive mobile menu
    - Auto-close menu on link clicks
    - Add tests for hamburger menu interaction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,66 @@
+import { useState } from 'react';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div data-role='Header' className='navbar-container'>
-    <div className='navbar'>
-      <img alt='Portfolio logo' src='/logo_dark.svg' className='navbar-logo' />
-      <div className='navbar-links'>
-        <a href='#home' className='navbar-link'>
-          Home
-        </a>
-        <a href='#services' className='navbar-link'>
-          Services
-        </a>
-        <a href='#experience' className='navbar-link'>
-          Experience
-        </a>
-        <a href='#contact' className='navbar-link'>
-          Contact
-        </a>
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div data-role='Header' className='navbar-container'>
+      <div className='navbar'>
+        <img
+          alt='Portfolio logo'
+          src='/logo_dark.svg'
+          className='navbar-logo'
+        />
+
+        <button
+          className='hamburger'
+          onClick={toggleMenu}
+          aria-label='Toggle navigation menu'
+          aria-expanded={isMenuOpen}
+        >
+          <svg
+            className={`hamburger-icon ${isMenuOpen ? 'active' : ''}`}
+            viewBox='0 0 100 100'
+            width='48'
+          >
+            <path
+              className='line top'
+              d='m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20'
+            />
+            <path className='line middle' d='m 30,50 h 40' />
+            <path
+              className='line bottom'
+              d='m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20'
+            />
+          </svg>
+        </button>
+
+        <div className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+          <a href='#home' className='navbar-link' onClick={closeMenu}>
+            Home
+          </a>
+          <a href='#services' className='navbar-link' onClick={closeMenu}>
+            Services
+          </a>
+          <a href='#experience' className='navbar-link' onClick={closeMenu}>
+            Experience
+          </a>
+          <a href='#contact' className='navbar-link' onClick={closeMenu}>
+            Contact
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
